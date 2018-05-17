684ffc91755b77476a88b4bfe5690d01b98a66ea
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-05-16 21:43:47 -0300

    update files

---
+++ TestaIdade.java
@@ -0,0 +1,17 @@
+public class TestaIdade {
+  
+public static void main (String [] args) {
+// declara um variavel do tipo inteiro, já inicializada
+int idade = 15;
+
+// imprime conteudo de idade
+System.out.println("Sua idade atual é: " + idade);
+
+// declara uma nova variavel
+int idadeNoAnoQueVem;
+
+// inicializa a variavel, que recebe idade + 1
+idadeNoAnoQueVem = idade + 1;
+System.out.println("Sua idade ano que vem será: "+ idadeNoAnoQueVem);
+}
+}
